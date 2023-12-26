cbd76ef4f22c5c9b4ac9c3282e213c7cd172daf8
Dima Satskevich <dima_satskevich@mail.ru>
2023-12-27 00:20:43 +0300

    feat: Добавил тестовые контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,29 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
 
 @RestController
 public class FilmController {
+    @PostMapping()
+    public ResponseEntity<?> addFilm(@RequestBody @Valid Film film) {
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> updateFilm(@RequestBody Film film) {
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getFilms(@RequestParam Integer id) {
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
@@ -9,7 +10,24 @@ import javax.validation.Valid;
 @RestController
 public class UserController {
 
-    public void createUser(@Valid @RequestBody User user) {
+    @PostMapping()
+    public ResponseEntity<?> addUser(@RequestBody @Valid User user) {
+        if(user.getName() == null || user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 
+    @PutMapping()
+    public ResponseEntity<?> updateUser(@RequestBody @Valid User user) {
+        if(user.getName() == null || user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getUsers() {
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
