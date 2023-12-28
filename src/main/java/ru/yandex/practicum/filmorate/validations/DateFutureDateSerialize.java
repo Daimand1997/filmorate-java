d0b22a3214f672799d4cc16fbf36095acc6e0855
Dmitriy <Diman4ik.1997@ya.ru>
2023-12-28 16:33:19 +0300

    fix: edit if

--- src/main/java/ru/yandex/practicum/filmorate/validations/DateFutureDateSerialize.java
+++ src/main/java/ru/yandex/practicum/filmorate/validations/DateFutureDateSerialize.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.validations;
 import com.fasterxml.jackson.databind.util.StdConverter;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
-import javax.swing.text.DateFormatter;
-import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -13,9 +11,9 @@ public class DateFutureDateSerialize extends StdConverter<Object, String> {
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
-    public String convert(Object value){
-        if(value instanceof LocalDate) {
-            if(LocalDate.parse("1895-12-28").isAfter((LocalDate)value)) {
+    public String convert(Object value) {
+        if (value instanceof LocalDate) {
+            if (LocalDate.parse("1895-12-28").isAfter((LocalDate) value)) {
                 throw new ValidationException("Дата не может быть раньше 28.12.1895");
             }
             return dateTimeFormatter.format((LocalDate) value);
